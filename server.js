f595a99fa963d86a70af7e1caa7dfed4d08ed742
Maciek Mielczarek <maciejka-67@o2.pl>
2022-01-20 08:38:11 +0100

    Add Handlebars templates

--- server.js
+++ server.js
@@ -1,41 +1,54 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const isLogged = () => false;
 
 const app = express();
 
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
-
 app.use('/user', (req, res, next) => {
-  isLogged() ? next() : res.show('login.html');
+  isLogged() ? next() : res.render('login');
 });
 
 app.get(/^\/(home)?$/, (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', {layout: 'dark'});
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
 });
 
 app.get('/user/settings', (req, res) => {
-  res.show('settings.html');
+  res.render('settings');
 });
 
 app.get('/user/panel', (req, res) => {
-  res.show('panel.html');
+  res.render('panel');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', {name: req.params.name});
 });
 
 app.use((req, res) => {
-  res.status(404).show('notFound.html');
+  res.status(404).render('notFound');
 })
 
 app.listen(8000, () => {
