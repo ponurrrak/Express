2f4360bf98a16069f1e30703eb5c743498be696e
Maciek Mielczarek <maciejka-67@o2.pl>
2022-01-20 19:20:19 +0100

    Add contact form and related code in server.js

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const formidable = require('formidable');
+const fs = require('fs');
 
 const isLogged = () => false;
+
+const publicDirPath = path.join(__dirname, 'public');
 
 const app = express();
 
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(publicDirPath));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  const contentType = req.headers['content-type'] || '';
+  if(req.method === 'POST' && contentType.startsWith('multipart/form-data')){
+    const form = formidable({
+      multiples: true,
+      uploadDir: path.join(publicDirPath, 'upload'),
+      keepExtensions: true,
+    });
+    form.parse(req, (err, fields, files) => {
+      req.body = fields;
+      req.files = files;
+      next();
+    });
+  } else {
+    next();
+  }
+});
 
 app.use('/user', (req, res, next) => {
   isLogged() ? next() : res.render('login');
@@ -45,6 +69,20 @@ app.get('/user/panel', (req, res) => {
 
 app.get('/hello/:name', (req, res) => {
   res.render('hello', {name: req.params.name});
+});
+
+app.post('/contact/send-message', (req, res) => {
+  const { author, sender, title, message } = req.body;
+  const { image } = req.files;
+  if(author && sender && title && message && image.originalFilename) {
+    res.render('contact', {
+      isSent: true,
+      filename: image.originalFilename,
+      savedFilename: image.newFilename,
+    });
+  } else {
+    res.render('contact', {isError: true})
+  }
 });
 
 app.use((req, res) => {
